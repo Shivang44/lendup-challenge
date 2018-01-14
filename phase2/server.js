9b1b9cdc3747e8eaa8706e6c7a5be06a4e5fd94b
Shivang44 <shivangs44@gmail.com>
2018-01-14 14:59:40 -0500

    Outgoing call implemented

--- phase2/server.js
+++ phase2/server.js
@@ -1,4 +1,4 @@
-const VoiceResponse = require('twilio').twiml.VoiceResponse;
+const Twilio = require('twilio');
 const Hapi = require('hapi');
 const Boom = require('boom');   /* Used to create standardized HTTP error responses. */
 const Joi = require('joi');     /* Used for input validation */
@@ -21,11 +21,31 @@ const routes = [
         method: 'POST',
         path: '/callUser',
         handler: function (request, response) {
-            return "yay";
+            let phone = request.payload.phone; /* Validated with Joi in config section below */
+
+            const accountSid = 'AC52b0186c5cc86941663afe7256a6d9d6';
+            const authToken = '61cfc3afa581d6ddc94b6af988fa9c20';
+            const client = new Twilio(accountSid, authToken);
+
+            return client.api.calls
+              .create({
+                url: 'http://demo.twilio.com/docs/voice.xml',
+                to: phone,
+                from: '+15103451850',
+              })
+              .then((call) => {
+                  return {
+                      msg: "Calling provided number!"
+                  };
+              })
+              .catch((error) => {
+                  return Boom.serverUnavailable("Unable to make call. Please try again.");
+              });
         },
         config: {
             validate: {
                 payload: {
+                    /* Ensure sure phone number is 10 digits long. */
                     phone: Joi.string().regex(/^\d{10}$/)
                 }
             }
